3a67740bdffbacc39636ccddb5450f4210b348aa
TuriAsim <65749273+TuriAsim@users.noreply.github.com>
2020-09-01 18:44:23 +0900

    updated readme file

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     Long_descripion_content_type = 'text/markdown',
     packages = setuptools.find_packegs(),
     classifiers = [
-    'programming language'::Python :: 3,
+    'Programming Language :: Python :: 3',
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent']
     python_requires = '>=3.5'
