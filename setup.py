86f17681ebaefe3d03ffeb88df2900bceb005866
TuriAsim <65749273+TuriAsim@users.noreply.github.com>
2020-09-02 17:23:52 +0900

    updated readme file

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setuptools.setup(
     classifiers = [
     'Programming Language :: Python :: 3',
     'License :: OSI Aproved :: MIT License',
-    'Operating System :: OS Independent']
+    "Operating System :: OS Independent"],
     python_requires = '>=3.5'
 	)
+	
